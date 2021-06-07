1c4bd60dad6cb4ba40feee3b8a6ea83b56061d75
WDRK <lllrrrlrssr@gmail.com>
2021-06-07 14:38:02 +0900

    Feat: 추가 DisplayNumber 컴포넌트의 컨테이너
    DisplayNumber 컴포넌트가 Redux에 종속하기 않기 위해

--- src/components/DisplayNumber.jsx
+++ src/components/DisplayNumber.jsx
@@ -1,23 +1,11 @@
 import React, { Component } from "react";
-import store from "../store";
 
 export default class DisplayNumber extends Component {
-  constructor(props) {
-    super(props);
-    store.subscribe(() => {
-      this.setState({
-        number: store.getState().number,
-      });
-    });
-  }
-  state = {
-    number: store.getState().number,
-  };
   render() {
     return (
       <div>
         <h1>Display Number</h1>
-        <input type="text" value={this.state.number} readOnly />
+        <input type="text" value={this.props.number} readOnly />
       </div>
     );
   }
---
+++ src/containers/DisplayNumber.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import DisplayNumber from "../components/DisplayNumber";
+import store from "../store";
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    store.subscribe(() => {
+      this.setState({
+        number: store.getState().number,
+      });
+    });
+  }
+  state = {
+    number: store.getState().number,
+  };
+  render() {
+    return <DisplayNumber number={this.state.number}></DisplayNumber>;
+  }
+}
